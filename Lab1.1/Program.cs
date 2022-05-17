d5b41779a0d5cdcd6404e1884160af9d312fde07
glebv <glebv@inbox.ru>
2022-05-17 17:23:19 +0300

    Add project files.

---
+++ Lab1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._1
+{
+    internal class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            Console.WriteLine("Enter your name:");
+            MyName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", MyName);
+        }
+    }
+}
