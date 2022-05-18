bac4a70e73200e2ff1457eb2282bbfe265188249
glebv <glebv@inbox.ru>
2022-05-18 13:21:22 +0300

    +Lab2

---
+++ Lab2.1/enum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._1
+{
+    public enum AccountType {Checking,Deposit};
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            Console.WriteLine("The customer account type is {0}", goldAccount);
+            Console.WriteLine("The customer account type is {0}", platinumAccount);
+        }
+    }
+}
---
+++ Lab2.2/struct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._2
+{
+    public enum AccountType { Checking, Deposit };
+    public struct BankAccount
+        {
+            public long accNo;
+            public decimal accBal;
+            public AccountType accType;
+        }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            goldAccount.accNo = 123;
+
+            Console.WriteLine("*** Account summary ***");
+            Console.WriteLine("Acct number {0}", goldAccount.accNo);
+            Console.WriteLine("Acct type {0}", goldAccount.accType);
+            Console.WriteLine("Acct ballance ${0}", goldAccount.accBal);
+        }
+    }
+}
