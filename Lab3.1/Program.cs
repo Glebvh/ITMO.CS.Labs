0d4b8a82c6741001e36631f3c69f87c03cfaa114
glebv <glebv@inbox.ru>
2022-05-22 19:01:13 +0300

    +3.2,3.3

--- Lab3.1/Program.cs
+++ Lab3.1/Program.cs
@@ -12,32 +12,73 @@ namespace Lab3._1
         {
             public enum monthName { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
             
+            
             static void Main(string[] args)
             {
-                Console.Write("Please enter the day number:");
+            try 
+           
+            {
+                Console.Write("Please enter the year: ");
                 string line = Console.ReadLine();
+                int yearNum = int.Parse(line);
+                bool isLeapYear = (yearNum % 4 == 0) && (yearNum % 100 != 0 || yearNum % 400 == 0);
+                    
+                    int maxDayNum = isLeapYear ? 366 : 365;
+                    Console.WriteLine("Enter the number of day between 1 and {0}", maxDayNum);
+                                        
+                line = Console.ReadLine();
                 int dayNum = int.Parse(line);
+
+                    if (dayNum < 1 || dayNum > maxDayNum)
+                    {
+                        throw new ArgumentOutOfRangeException("Day out of range");
+                    }
+
 
                 int monthNum = 0;
                 var daysInMonths = new List<int> { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-                foreach (int daysInMonth in daysInMonths)
-                {
-                    if (dayNum <= daysInMonth)
+                var daysInLeapMonths = new List<int> { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+                    if (isLeapYear) 
                     {
-                        break;
+                      foreach (int daysInMonth in daysInLeapMonths)
+                      {
+                            if (dayNum <= daysInMonth)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                dayNum = dayNum - daysInMonth;
+                                monthNum++;
+                            }
+                      }
                     }
-                    else
+                else
                     {
-                        dayNum = dayNum - daysInMonth;
-                        monthNum++;
+                        foreach (int daysInMonth in daysInMonths)
+                        {
+                            if (dayNum <= daysInMonth)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                dayNum = dayNum - daysInMonth;
+                                monthNum++;
+                            }
+                        }
                     }
-                }
                 
-           
                 monthName temp = (monthName)monthNum;
                 string monthName = temp.ToString();
 
                 Console.WriteLine("{0} {1}", dayNum, monthName);
+            
+             }
+                catch (Exception caught) {
+                    Console.WriteLine (caught);
+                        }
 
             }
         }
