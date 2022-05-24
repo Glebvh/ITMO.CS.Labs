586dbbce2a469d85f4e71940feff1e5a49fbec86
glebv <glebv@inbox.ru>
2022-05-25 02:46:02 +0300

    +Lab5

---
+++ Lab5/ITMO.CSdev.Lab5.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ITMO.CSdev.LabFive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////// не работает  
+            string fileName = args[0];
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            int size = (int)stream.Length;
+            char[] contents = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                contents[i] = (char)reader.Read();
+            }
+            //foreach (char ch in contents)
+            //{
+            //    Console.Write(ch);
+            //}
+            reader.Close();
+            Summ(contents);
+        }
+        static void Summ(char[] contents)
+        {
+            int vovels = 0, consonants = 0, lines = 0;
+
+            foreach (char current in contents)
+            {
+                if (char.IsLetter(current))
+                {
+                    if ("AEIOUaeiou".IndexOf(current) != -1)
+                    {
+                        vovels++;
+                    }
+                    else
+                    {
+                        consonants++;
+                    }
+                }
+                else if (current == '\n')
+                {
+                    lines++;
+                }
+                Console.WriteLine("Total no of characters: {0}", contents.Length);
+                Console.WriteLine("Total no of vowels : {0}", vovels);
+                Console.WriteLine("Total no of consonants: {0}", consonants);
+                Console.WriteLine("Total no of lines : {0}", lines);
+            }
+        }
+    }
+}
---
+++ Lab5/ITMO.CSdev.Lab5.2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSdev.LabFive.second
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] a = new int[2, 2];
+            int[,] b = new int[2, 2];
+            input(a);
+            input(b);
+            multiply(a, b);
+        }
+        static void output(int[,] result)
+        {
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static int[,] multiply (int [,] a, int [,] b)
+        {
+            int[,] result = new int[2, 2];
+            for (int r = 0; r < 2; r++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    result[r, c] = a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+
+
+            //result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0];
+            //result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
+            //result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0];
+            //result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
+            output(result);
+            return result;
+
+        }
+        static void input (int[,] dst)
+        {
+            for (int r = 0; r < 2; r++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    Console.Write("Enter the value for [{0},{1}]: ", r,c);
+                    string s = Console.ReadLine();
+                    dst[r, c] = int.Parse(s);
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
