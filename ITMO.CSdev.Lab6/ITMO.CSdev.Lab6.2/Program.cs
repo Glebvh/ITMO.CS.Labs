deb9477d3f9b623d9d6239fefe2581bb81bccac1
glebv <glebv@inbox.ru>
2022-05-27 01:06:18 +0300

    +Lab6.3

--- ITMO.CSdev.Lab6/ITMO.CSdev.Lab6.2/Program.cs
+++ ITMO.CSdev.Lab6/ITMO.CSdev.Lab6.2/Program.cs
@@ -12,9 +12,10 @@ namespace ITMO.CSdev.LabSix.F
         private long accNo;
         private decimal accBal;
         private AccountType accType;
-        public void Populate(long number, decimal balance)
+        public static long nextAccNo = 123;
+        public void Populate(decimal balance)
         {
-            accNo = number;
+            accNo = NextNumber();
             accBal = balance;
             accType = AccountType.Checking;
         }
@@ -30,15 +31,68 @@ namespace ITMO.CSdev.LabSix.F
         {
             return accType.ToString();
         }
+
+        private static long NextNumber()
+        {
+            return nextAccNo++;
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            accBal += amount;
+            if (accBal < 0)
+            {
+                Console.WriteLine("Warning");
+            }
+            return accBal;
+        }
+        public bool WithDraw(decimal amount)
+        {
+            bool suficientFunds = accBal >= amount;
+            if (suficientFunds)
+            {
+                accBal -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Balance is low: " + accBal);
+            }
+            
+            return suficientFunds;
+        }
+        public static void TestWithdraw(BankAccount acc)
+        {
+            Console.Write("Enter required sum: ");
+            decimal amount = decimal.Parse(Console.ReadLine());
+            if (!acc.WithDraw(amount))
+            {
+                Console.WriteLine("Insufficient funds");
+            }
+        }
+        
     }
     class CreateAccount
     {
+        public static void TestDeposit(BankAccount acc)
+        {
+            Console.Write("Enter amount to deposit: ");
+            decimal amount = decimal.Parse(Console.ReadLine());
+            acc.Deposit(amount);
+        }
         static void Main(string[] args)
         {
             BankAccount berts = NewBankAccount();
             Write(berts);
+            TestDeposit(berts);
+            Write(berts);
+            BankAccount.TestWithdraw(berts);
+            Write(berts);
 
             BankAccount freds = NewBankAccount();
+            Write(freds);
+            TestDeposit(freds);
+            Write(freds);
+            BankAccount.TestWithdraw(freds);
             Write(freds);
         }
 
@@ -46,13 +100,10 @@ namespace ITMO.CSdev.LabSix.F
         {
             BankAccount created = new BankAccount();
 
-            Console.Write("Enter the account number: ");
-            long number = long.Parse(Console.ReadLine());
-
             Console.Write("Enter the account balance!: ");
             decimal balance = decimal.Parse(Console.ReadLine());
 
-            created.Populate(number, balance);
+            created.Populate(balance);
 
             return created;
         }
