a324e2f3c5992d25faae6cb3a0bd8f1176edda85
glebv <glebv@inbox.ru>
2022-05-29 22:35:40 +0300

    +Lab7.2

---
+++ ITMO.CSdev.Lab7/ITMO.CS.Labs.Lab7.2/CodeFile1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter string to reverse: ");
+        string x = Console.ReadLine();
+        ITMO.CS.Labs.LabSevenTwo.Utils.Reverse(ref x);
+        Console.WriteLine(x);
+    }
+}
---
+++ ITMO.CSdev.Lab7/ITMO.CS.Labs.Lab7.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CS.Labs.LabSevenTwo
+{
+    class Utils
+    {
+        public static void Reverse(ref string f)
+        {
+            string sRev = "";
+            for (int k = f.Length - 1; k >= 0; k--)
+            {
+                sRev = sRev + f[k];
+            }
+            f = sRev;
+        }
+        
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public static int Greeter(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            try
+            {
+                int x;
+                int y;
+                Console.Write("Enter the first: ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("Enter the second: ");
+                y = int.Parse(Console.ReadLine());
+                int greeter;
+                greeter = Utils.Greeter(x, y);
+                Console.WriteLine("The greeter = " + greeter);
+                Console.WriteLine("Before swap: " + x, +y);
+                Utils.Swap(ref x, ref y);
+                Console.WriteLine("After: " + x, +y);
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
